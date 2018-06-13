7f174e385ea3952674fe2d7f40265b3bed7c3829
mogge <muzigen@gmail.com>
2018-06-14 08:34:55 +0900

    create mage build

--- magefile.go
+++ magefile.go
@@ -4,40 +4,20 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-
-	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	"github.com/magefile/mage/sh"
 )
 
 // Default target to run when none is specified
 // If not set, running mage will list available targets
 // var Default = Build
 
-// A build step that requires additional params, or platform specific steps for example
 func Build() error {
-	mg.Deps(InstallDeps)
 	fmt.Println("Building...")
-	cmd := exec.Command("go", "build", "-o", "MyApp", ".")
-	return cmd.Run()
-}
-
-// A custom install step if you need your bin someplace other than go/bin
-func Install() error {
-	mg.Deps(Build)
-	fmt.Println("Installing...")
-	return os.Rename("./MyApp", "/usr/bin/MyApp")
-}
-
-// Manage your deps, or running package managers.
-func InstallDeps() error {
-	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
-	return cmd.Run()
-}
-
-// Clean up after yourself
-func Clean() {
-	fmt.Println("Cleaning...")
-	os.RemoveAll("MyApp")
+	env := map[string]string{
+		"GOARCH":"amd64",
+		"GOOS":"linux",
+	}
+	hash, err := sh.OutputWith(env,"go","build","myfirestore.go","struct.go")
+	fmt.Println(hash)
+	return err
 }
